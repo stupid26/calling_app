9720d55d3ed46d70205e939e6605fbbe651e1ef0
talhawasi1@gmail.com <Talha@wasi1>
2020-08-17 08:33:28 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:calling_app/resources/firebase_repository.dart';
 import 'package:calling_app/screens/home_screen.dart';
 import 'package:calling_app/screens/login_screen.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:calling_app/resources/firebase_repository.dart';
 
@@ -11,13 +12,297 @@ class LoginScreen extends StatefulWidget {
 
 class LoginScreenState extends State<LoginScreen> {
   FirebaseRepository _repository = FirebaseRepository();
+  int _pageState = 0;
+  var _backgroundColor = Colors.white;
+  var _headingColor = Color(0xFFB40284A);
+  double _loginYOffset = 0;
+  double _loginOpacity = 1;
+  double _loginXOffset = 0;
+  double _loginWidth = 0;
+  double _loginHeight = 0;
+  double _registerYOffset = 0;
+  double windowWidth = 0;
+  double windowHeight = 0;
+  double _headingTop = 100;
+  double _registerHeight = 0;
+  bool _keyboardVisible = false;
+
+//  @override
+//  void initState() {
+//    super.initState();
+//    KeyboardVisibilityNotification().addNewListener(
+//      onChange: (bool visible) {
+//        setState(() {
+//          _keyboardVisible = visible;
+//        });
+//      },
+//    );
+//  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: loginButton()
-      );
+    windowHeight = MediaQuery.of(context).size.height;
+    windowWidth = MediaQuery.of(context).size.width;
+    _loginHeight = windowWidth + 220;
+    _registerHeight = windowWidth + 220;
+
+    switch (_pageState) {
+      case 0:
+        _backgroundColor = Colors.white;
+        _headingColor = Color(0xFFB40284A);
+
+        _headingTop = 100;
+
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+
+        _loginYOffset = windowHeight;
+        _loginHeight = _keyboardVisible ? windowHeight : windowHeight - 270;
+
+        _loginXOffset = 0;
+        _registerYOffset = windowHeight;
+        break;
+      case 1:
+        _backgroundColor = Color(0xFFBD34C59);
+        _headingColor = Colors.white;
+
+        _headingTop = 90;
+
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+
+        _loginYOffset = _keyboardVisible ? 40 : 270;
+        _loginHeight = _keyboardVisible ? windowHeight : windowHeight - 270;
+
+        _loginXOffset = 0;
+        _registerYOffset = windowHeight;
+        break;
+      case 2:
+        _backgroundColor = Color(0xFFBD34C59);
+        _headingColor = Colors.white;
+
+        _headingTop = 80;
+
+        _loginWidth = windowWidth - 40;
+        _loginOpacity = 0.7;
+
+        _loginYOffset = _keyboardVisible ? 30 : 240;
+        _loginHeight = _keyboardVisible ? windowHeight : windowHeight - 240;
+
+        _loginXOffset = 20;
+        _registerYOffset = _keyboardVisible ? 55 : 270;
+        _registerHeight = _keyboardVisible ? windowHeight : windowHeight - 270;
+        break;
+    }
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Nunito',
+      ),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            AnimatedContainer(
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 1000),
+              color: _backgroundColor,
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _pageState = 0;
+                      });
+                    },
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          AnimatedContainer(
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            duration: Duration(milliseconds: 1000),
+                            margin: EdgeInsets.only(
+                              top: _headingTop,
+                            ),
+                            child: Text(
+                              'Callingo',
+                              style: TextStyle(
+                                fontSize: 28,
+                                color: _headingColor,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(25),
+                            padding: EdgeInsets.symmetric(horizontal: 32),
+                            child: Text(
+                              'We make In Organisation Communication more effecient and safe ~ xbotics7',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: _headingColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      bottom: 60,
+                      top: 40,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 32),
+                    child: Center(
+                      child: Image.asset('assets/images/splash_bg.png'),
+                    ),
+                  ),
+                  Container(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_pageState != 0) {
+                            _pageState = 0;
+                          } else {
+                            _pageState = 1;
+                          }
+                        });
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(32),
+                        padding: EdgeInsets.all(20),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Color(0xFFB40284A),
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Center(
+                          child: Text(
+                            'Get Started',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            AnimatedContainer(
+              padding: EdgeInsets.all(32),
+              height: _loginHeight,
+              width: _loginWidth,
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 1000),
+              transform:
+                  Matrix4.translationValues(_loginXOffset, _loginYOffset, 1),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(_loginOpacity),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: Text(
+                          'Login Into Your Account',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      InputWithIcon(hint: 'Enter Email',icon: Icons.email,),
+                      SizedBox(height: 20),
+                      InputWithIcon(hint: 'Enter Password', icon: Icons.vpn_key,),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      PrimaryButton(btnText: 'Login'),
+                      SizedBox(height: 20),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _pageState = 2;
+                          });
+                        },
+                        child: OutlineButton(
+                          btnText: 'Create Account',
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            AnimatedContainer(
+              height: _registerHeight,
+              padding: EdgeInsets.all(32),
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 500),
+              transform: Matrix4.translationValues(0, _registerYOffset, 1),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: Text(
+                          'Create a New Account',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      InputWithIcon(hint: 'Enter Email', icon: Icons.email,),
+                      SizedBox(height: 20),
+                      InputWithIcon(hint: 'Enter Password', icon: Icons.vpn_key,),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            performLogin();
+                          });
+                        },
+                        child: PrimaryButton(
+                          btnText: 'Create Account',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _pageState = 1;
+                          });
+                        },
+                        child: OutlineButton(
+                          btnText: 'Back to Login',
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  //Login Page logic
 
   Widget loginButton() {
     return FlatButton(
@@ -49,15 +334,122 @@ class LoginScreenState extends State<LoginScreen> {
         _repository.addDataToDb(user).then((value) {
           Navigator.pushReplacement(context,
               MaterialPageRoute(builder: (context) {
-                return HomeScreen();
-              }));
+            return HomeScreen();
+          }));
         });
       } else {
         Navigator.pushReplacement(context,
             MaterialPageRoute(builder: (context) {
-              return HomeScreen();
-            }));
+          return HomeScreen();
+        }));
       }
     });
+  }
+}
+
+//PAGE UI MODULES
+
+class PrimaryButton extends StatefulWidget {
+  final String btnText;
+  PrimaryButton({this.btnText});
+
+  @override
+  _PrimaryButtonState createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFFB40284A),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
+
+class OutlineButton extends StatefulWidget {
+  final String btnText;
+  OutlineButton({this.btnText});
+
+  @override
+  _OutlineButtonState createState() => _OutlineButtonState();
+}
+
+class _OutlineButtonState extends State<OutlineButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color(0xFFB40284A),
+          width: 2,
+        ),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(
+            color: Color(0xFFB40284A),
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InputWithIcon extends StatefulWidget {
+  final IconData icon;
+  final String hint;
+  InputWithIcon({this.icon, this.hint});
+
+  @override
+  _InputWithIconState createState() => _InputWithIconState();
+}
+
+class _InputWithIconState extends State<InputWithIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+              color: Color(0xFFBC7C7C7),
+              width: 2
+          ),
+          borderRadius: BorderRadius.circular(50)
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: 60,
+              child: Icon(
+                widget.icon,
+                size: 20,
+                color: Color(0xFFBB9B9B9),
+              )
+          ),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 20),
+                  border: InputBorder.none,
+                  hintText: widget.hint
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
